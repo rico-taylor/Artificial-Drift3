73e9f654ecb258cf813197ea88849a2526f1692d
Rico Taylor <ritayl@my.rongotai.school.nz>
2024-07-01 11:14:51 +1200

    new edits

--- code/main.py
+++ code/main.py
@@ -10,22 +10,23 @@ from math import sin, cos, atan, radians, sqrt, tanh
 window = pyglet.window.Window(resizable = False, caption="Artificial Drift")
 window.set_fullscreen(True)
 
-#entry screen
-entryWindow = pyglet.window.Window(caption="Artificial Drift")
-entryWindow.set_fullscreen(True)
+windowOn = [1,0]
 
+#defining batches
+lines = pyglet.graphics.Batch()
+displays = pyglet.graphics.Batch()
 entryDisplay = pyglet.graphics.Batch()
 
 #entry images
 logo_img = image.load("images/logo_finished.png")
 logo_img.anchor_x = logo_img.width//2
 logo_img.anchor_y = logo_img.height//2
-logo = sprite.Sprite(logo_img, x=entryWindow.width//2, y=entryWindow.height//2 +100, batch=entryDisplay)
+logo = sprite.Sprite(logo_img, x=window.width//2, y=window.height//2 +100, batch=entryDisplay)
 
 play_img = image.load("images/text_play.png")
 play_img.anchor_x = play_img.width//2
 play_img.anchor_y = play_img.height//2
-playButton = sprite.Sprite(play_img, x=entryWindow.width//2, y=entryWindow.height//2 -200, batch=entryDisplay)
+playButton = sprite.Sprite(play_img, x=window.width//2, y=window.height//2 -200, batch=entryDisplay)
 playButton.scale = 0.5
 
 triangle_img = image.load("images/triangle1_translucent.png")
@@ -34,21 +35,6 @@ triangle_img.anchor_y = triangle_img.height//2
 triangle1 = sprite.Sprite(triangle_img, x=100, y=100, batch=entryDisplay)
 triangle1.rotation = 30
 triangle1.scale = 0.3
-
-@entryWindow.event
-def on_draw():
-  entryWindow.clear()
-  entryDisplay.draw()
-
-
-@entryWindow.event
-def on_mouse_press(x,y,button,modifiers):
-  if button == mouse.LEFT:
-    entryWindow.close()
-    return None
-
-lines = pyglet.graphics.Batch()
-displays = pyglet.graphics.Batch()
 
 #track_image = image.load("images/track V4.png")
 car_image = image.load("images/car.png")
@@ -366,15 +352,17 @@ drift_time = 8
 @window.event
 def on_draw():
   window.clear()
-  displays.draw()
-  #lap_displays() this function is commented out since it was causing lag
-  #track.draw()
-  car.draw()
-  lines.draw()
-  circle.draw()
-  circle1.draw()
-  circle2.draw()
-  circle3.draw()
+  if windowOn[0] == 1:
+    entryDisplay.draw()
+  if windowOn[1] == 1:
+    displays.draw()
+    #lap_displays() this function is commented out since it was causing lag
+    car.draw()
+    lines.draw()
+    circle.draw()
+    circle1.draw()
+    circle2.draw()
+    circle3.draw()
 
 @window.event
 def on_key_press(symbol, modifiers):
@@ -407,7 +395,10 @@ def on_key_press(symbol, modifiers):
 
 @window.event   
 def on_mouse_press(x,y,button, modifiers):
+  global windowOn
   if button == mouse.LEFT:
+    if windowOn == [1,0]:
+      windowOn = [0,1]
     print(x,y)
     print(lap_list)
 
