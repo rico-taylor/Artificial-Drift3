a0e30ccbc64f28f0bbba2e995f170f53891006dd
Rico Taylor <ritayl@my.rongotai.school.nz>
2024-08-01 15:13:46 +1200

    classes work

--- code/classes.py
+++ code/classes.py
@@ -26,28 +26,62 @@ class Car:
 
         self.velocity = 0
         self.forward = False
+        self.backward = False
+        self.clockwise = False
+        self.aclockwise = False
+
+        self.rotation_speed = 3
         self.acceleration = 0.1 *scale_factor
     
-    def on_key_press(self,symbol,modifiers):
+    def on_key_press(self, symbol,modifiers):
         if symbol == key.UP:
             self.forward = True
-    
-    def update(self):
+        if symbol == key.DOWN:
+            self.backward = True
+        if symbol == key.LEFT:
+            self.aclockwise = True
+        if symbol == key.RIGHT:
+            self.clockwise = True
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == key.UP:
+            self.forward = False
+        if symbol == key.DOWN:
+            self.backward = False
+        if symbol == key.LEFT:
+            self.aclockwise = False
+        if symbol == key.RIGHT:
+            self.clockwise = False
+
+
+    def update(self, dt):
         if self.forward == True:
             self.velocity += self.acceleration
+        if self.backward == True:
+            self.velocity -= self.acceleration/1.3
         
-            self.dy = self.velocity * cos(radians(self.car.rotation))
-            self.dx = self.velocity * sin(radians(self.car.rotation))
+        if self.aclockwise == True:
+            self.car.rotation -= self.rotation_speed
+        if self.clockwise == True:
+            self.car.rotation += self.rotation_speed
 
-            self.car.x += self.dx
-            self.car.y += self.dy
+        dy = self.velocity * cos(radians(self.car.rotation))
+        dx = self.velocity * sin(radians(self.car.rotation))
+
+        self.car.y += dy
+        self.car.x += dx
 
 player1 = Car(100,100,260,"images/car.png")
 
 @window.event
 def on_draw():
     window.clear()
-    player1.update()
     player1.car.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    player1.on_key_press(symbol, modifiers)
+
+pyglet.clock.schedule_interval(player1.update, 1/60)
 
 pyglet.app.run()
