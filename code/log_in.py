dc337e87125cc89442787de7eb055e593f8c54bb
Rico Taylor <ritayl@my.rongotai.school.nz>
2024-09-27 12:29:34 +1200

    edits

--- code/log_in.py
+++ code/log_in.py
@@ -5,7 +5,11 @@ from pyglet import sprite, image
 from pyglet.window import key, mouse
 from math import sin, cos, atan, acos, asin, radians, sqrt, tanh
 
-text_input = str()
+text_input1 = str()
+text_input2 = str()
+text_input3 = str()
+
+selected_textbox = 1
 
 #main window
 window = pyglet.window.Window(resizable = False, caption="Artificial Drift")
@@ -49,12 +53,36 @@ username_img.anchor_y = username_img.height//2
 usernameHeading = sprite.Sprite(username_img, x=windowwidth//2-200, y=windowheight//2 +140, batch=loginDisplays)
 usernameHeading.scale = 0.1*scale_factor
 
+#"password"
+password_img = image.load("images/text_password.png")
+password_img.anchor_x = password_img.width//2
+password_img.anchor_y = password_img.height//2
+passwordHeading = sprite.Sprite(password_img, x=windowwidth//2-200, y=windowheight//2 +30, batch=loginDisplays)
+passwordHeading.scale = 0.1*scale_factor
+
+#"confirm password"
+confirm_img = image.load("images/text_confirm-password.png")
+confirm_img.anchor_x = confirm_img.width//2
+confirm_img.anchor_y = confirm_img.height//2
+confirmHeading = sprite.Sprite(confirm_img, x=windowwidth//2-140, y=windowheight//2 -80, batch=loginDisplays)
+confirmHeading.scale = 0.15*scale_factor
+
 #textbox1
 rectangle3 = pyglet.shapes.Rectangle(x=450, y=520, width=500, height=50, color=(255, 255, 255), batch=loginDisplays)
 rectangle3.opacity = 50
 
+#textbox2
+rectangle5 = pyglet.shapes.Rectangle(x=450, y=410, width=500, height=50, color=(255, 255, 255), batch=loginDisplays)
+rectangle5.opacity = 50
+
+#textbox3
+rectangle6 = pyglet.shapes.Rectangle(x=450, y=300, width=500, height=50, color=(255, 255, 255), batch=loginDisplays)
+rectangle6.opacity = 50
+
 #label
-label = pyglet.text.Label(text_input, font_name='Arial', font_size=20, x=700, y=800, batch=loginDisplays)
+label1 = pyglet.text.Label(text_input1, font_name='Arial', font_size=20, x=700, y=700, batch=loginDisplays)
+label2 = pyglet.text.Label(text_input2, font_name='Arial', font_size=20, x=700, y=700, batch=loginDisplays)
+label3 = pyglet.text.Label(text_input3, font_name='Arial', font_size=20, x=700, y=700, batch=loginDisplays)
 
 
 @window.event
@@ -64,29 +92,75 @@ def on_draw():
 next_letter = False
 @window.event
 def on_key_press(symbol,modifiers):
-    global text_input
+    global text_input1
+    global text_input2
+    global text_input3
+    global selected_textbox
     global next_letter
-    global label
-    if symbol == key.LSHIFT or symbol == key.RSHIFT:
-        next_letter = True
-    if next_letter == True:
-        if key.A <= symbol <= key.Z:
-            text_input = text_input + str(chr(symbol)).upper()
-            next_letter = False
+    global label1
+    global label2
+    global label3
+    if selected_textbox == 1:
+        if symbol == key.LSHIFT or symbol == key.RSHIFT:
+            next_letter = True
+        if next_letter == True:
+            if key.A <= symbol <= key.Z:
+                text_input1 = text_input1 + str(chr(symbol)).upper()
+                next_letter = False
+        else:
+            if symbol == key.BACKSPACE:
+                text_input1 = text_input1[:-1]
+            elif key.A <= symbol <= key.Z or symbol == key.SPACE:
+                text_input1 = text_input1 + str(chr(symbol))
+
+        label1 = pyglet.text.Label(text_input1, font_name='Arial', font_size=20, x=460, y=535, batch=loginDisplays)
+    elif selected_textbox == 2:
+        if symbol == key.BACKSPACE:
+            text_input2 = text_input2[:-1]
+        else:
+            text_input2 = text_input2 + str(chr(symbol))
+
+        label2 = pyglet.text.Label(text_input2, font_name='Arial', font_size=20, x=460, y=425, batch=loginDisplays)
+        rectangle3.color = (255,255,255)
+        rectangle5.color = (0, 0, 0)
+        rectangle6.color = (255, 255, 255)
     else:
         if symbol == key.BACKSPACE:
-            text_input = text_input[:-1]
-        elif key.A <= symbol <= key.Z or symbol == key.SPACE:
-            text_input = text_input + str(chr(symbol))
+            print("here")
+            text_input2 = text_input3[:-1]
+        else:
+            text_input3 = text_input3 + str(chr(symbol))
 
-    label = pyglet.text.Label(text_input, font_name='Arial', font_size=20, x=500, y=500, batch=loginDisplays)
+        label3 = pyglet.text.Label(text_input3, font_name='Arial', font_size=20, x=460, y=315, batch=loginDisplays)
+        rectangle3.color = (255,255,255)
+        rectangle5.color = (255, 255, 255)
+        rectangle6.color = (0, 0, 0)
 
 
 @window.event
 def on_mouse_press(x,y,button, modifiers):
-    global text_input
+    global text_input1
+    global selected_textbox
     if button == mouse.LEFT:
-        print(text_input)
-        text_input = str()
+        if 449 < x < 951:
+            if 519 < y < 571:
+                selected_textbox = 1
+                rectangle3.color = (0,0,0)
+                rectangle5.color = (255, 255, 255)
+                rectangle6.color = (255, 255, 255)
+            elif 409 < y < 451:
+                selected_textbox = 2
+                rectangle3.color = (255,255,255)
+                rectangle5.color = (0, 0, 0)
+                rectangle6.color = (255, 255, 255)
+            elif 299 < y < 351:
+                selected_textbox = 3
+                rectangle3.color = (255,255,255)
+                rectangle5.color = (255, 255, 255)
+                rectangle6.color = (0, 0, 0)
+
+
+        
+        print(x,y)
 
 pyglet.app.run()
